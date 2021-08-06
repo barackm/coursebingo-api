bc3389479db1daf3a7c9c50f2974d46f7f39df97
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-06 14:11:10 +0200

    Add the create course method

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,27 @@ class CoursesController < ApplicationController
     def index 
         render json: Course.all
     end
+
+    def show
+        course ||= Course.find_by(id: params[:id])
+        if course.nil?
+            render json: {error: "Course not found with ID #{params[:id]}"}, status: 404
+        else
+            render json: course
+        end
+    end
+
+    def create 
+        course = Course.new(course_params)
+        if course.save
+            render json: course
+        else
+            render json: {error: course.errors.full_messages}, status: 422
+        end
+    end
+
+    private
+    def course_params 
+      params.require(:course).permit(:name, :description, :duration, :price, :image, :author_id)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_111527) do
+ActiveRecord::Schema.define(version: 2021_08_06_115859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_111527) do
     t.string "name"
     t.integer "author_id"
     t.string "price"
-    t.text "about"
+    t.text "description"
     t.string "duration"
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
