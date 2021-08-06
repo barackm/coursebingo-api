7e949d6ba60691a7f600efbbea1b3fe55799ae59
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-06 13:12:01 +0200

    Create the user model serilizer and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_105931) do
+ActiveRecord::Schema.define(version: 2021_08_06_111023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,17 @@ ActiveRecord::Schema.define(version: 2021_08_06_105931) do
     t.text "about"
     t.string "duration"
     t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.boolean "is_admin"
+    t.string "avatar"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
