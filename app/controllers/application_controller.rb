d0c08c16f4dd932036d640068a195aba649849dc
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-07 16:09:33 +0200

    Update status code in sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::API
 
   def authorize_request
     @current_user = AuthorizeApiRequest.new(request.headers).call
-    render json: { message: 'Not Authorized' }, status: 401 unless @current_user
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
 
   def authorize_admin
     @admin = AuthorizeAdmin.new(current_user).call
-    render json: { message: 'You are not Authorized to perform that action' }, status: 401 unless @admin
+    render json: { error: 'You are not Authorized to perform that action' }, status: 401 unless @admin
   end
 end
